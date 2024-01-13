d3c9c46c474ad048532d02113a5ed6f5237e65a2
TaynaKikuti <143554145+TaynaKikuti@users.noreply.github.com>
2024-01-13 15:24:40 -0300

    Corrigindo bugs e adaptando telas

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ const inputAdicionar = document.querySelector('#inserir__produtos__input')
 const btnAdicionar = document.querySelector('.btnAdicionar')
 const listaDeProdutos = document.querySelector('.lista__de__compras')
 const listaDeComprados = document.querySelector('.comprados__lista')
-const btnDeletar = document.querySelectorAll('#delete')
-const btnComprado = document.querySelectorAll('#btnComprado')
-
+const listaLocalStorage = localStorage.getItem('lista')
 
 form.addEventListener('submit', function(evento){
     evento.preventDefault()
@@ -16,7 +14,16 @@ form.addEventListener('submit', function(evento){
     inputAdicionar.value = ''
 })
 
+function atualizaLista() {
+    localStorage.setItem('lista', JSON.stringify(listaDeItens))
+}
 
+if(listaLocalStorage) {
+    listaDeItens = JSON.parse(listaLocalStorage)
+    mostrarItem()
+} else {
+    listaDeItens = []
+}
 
 function salvarItem() {
     const comprasItem = inputAdicionar.value
@@ -26,35 +33,72 @@ function salvarItem() {
         alert('Item já existe na lista')
     } else {
         listaDeItens.push({
-            valor: inputAdicionar.value
+            valor: comprasItem,
+            checar: false
         })
         mostrarItem()
     }
 }
 
 
-function mostrarItem () {
-    listaDeProdutos.innerHTML += `
-    <li>
-    <div class="lista__item">
-        <label for="iproduto">${inputAdicionar.value}</label>
-        <input type="checkbox" name="iproduto" id="iproduto" class="checkbox__produto">
-    </div>
 
-    <div class="lista__item__img">
-        <img src="./assets/icons8-caixa-de-seleção-marcada-48.png" alt="Ícone de caixa de seleção para item comprado" width="30px" id="btnComprado">
-        <img src="./assets/icons8-remover-48.png" alt="ícone para remover" width="30px" id="delete">
-    </div>
-    </li>`
-    
-    
-    btnComprado.forEach(botao => {
-        botao.addEventListener('click', ()=> {
+function mostrarItem () {
+    listaDeProdutos.innerHTML = ''
+    listaDeComprados.innerHTML = ''
+    listaDeItens.forEach((elemento, index) => {
+        if(elemento.checar) {
             listaDeComprados.innerHTML += `
-            <div>${inputAdicionar.value}</div>
-            <div><img src="./assets/icons8-remover-48.png" alt="ícone para remover" width="30px" id="delete"></div>
+            <li data-value="${index}">
+                <div class="lista__item">
+                    <label for="iproduto">${elemento.valor}</label>
+                    <input type="checkbox" name="iproduto" id="iproduto" class="checkbox__produto">
+                </div>
+        
+                <div class="lista__item__img">
+                    <img src="./assets/icons8-remover-48.png" alt="ícone para remover" width="30px" id="delete">
+                </div>
+            </li>
             `
+        } else {
+            listaDeProdutos.innerHTML += `
+            <li data-value="${index}">
+                <div class="lista__item">
+                    <label for="iproduto">${elemento.valor}</label>
+                    <input type="checkbox" name="iproduto" id="iproduto" class="checkbox__produto">
+                </div>
+        
+                <div class="lista__item__img">
+                    <img src="./assets/icons8-caixa-de-seleção-marcada-48.png" alt="Ícone de caixa de seleção para item comprado" width="30px" id="btnComprado">
+                    <img src="./assets/icons8-remover-48.png" alt="ícone para remover" width="30px" id="delete">
+                </div>
+            </li>
+            `
+        }
+    })    
+
+
+    const btnDeletar = document.querySelectorAll('#delete')
+    btnDeletar.forEach(botao => {
+        botao.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            console.log(valorDoElemento)
+
+            listaDeItens.splice(valorDoElemento,1)
+            mostrarItem()
         })
     })
+
+
+
+    const btnComprado = document.querySelectorAll('#btnComprado')
+    btnComprado.forEach(botao => {
+        botao.addEventListener('click', (evento)=> {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens[valorDoElemento].checar = 'true'
+            mostrarItem()
+        })
+    })
+
+    atualizaLista()
 }
 
